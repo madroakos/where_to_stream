3a42e9959a626cb31921b82771c6f262df615afc
madroakos <madroboy@protonmail.com>
2024-08-26 09:30:53 +0200

    use Daisyui for the movie cards

--- src/Content.tsx
+++ src/Content.tsx
@@ -11,11 +11,13 @@ interface ContentProps {
 function Content(props: ContentProps) {
 
     return (
-        <div className="movieCard m-4 flex flex-col items-center justify-end transition-opacity">
-            <div className="w-56 self-start">
+        <div className="movieCard card m-4 flex flex-col items-center justify-end transition-opacity">
+            <div className="card shadow-lg">
+                <figure className="w-56 self-start">
                 <img src={props.title.poster} alt={props.title.title} className="mb-2"/>
-            </div>
-                <p className="text-wrap w-56 text-center h-10 align-bottom">{props.title.title}</p>
+            </figure>
+            <div className="card-body w-56 text-center items-center flex-grow-0 h-60 content-around">
+                <p className="card-title text-wrap">{props.title.title}</p>
                 <p className="text-gray-400">{props.title.year}</p>
                 <h2>Streaming on:</h2>
                 {props.title.availableOn?.length !== 0 ?
@@ -28,7 +30,8 @@ function Content(props: ContentProps) {
                         )}
                     </> : <p className="mb-1 mt-1">None</p>
                 }
-
+            </div>
+            </div>
         </div>
     )
 }
